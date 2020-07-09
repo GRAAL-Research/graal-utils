dbd755a687d350160d0b2e9085929bb4ef8b1eb0
David Beauchemin <david.beauchemin.5@ulaval.ca>
2020-07-09 11:20:55 -0400

    Update __init__.py

--- graal_utils/__init__.py
+++ graal_utils/__init__.py
@@ -1,4 +1,3 @@
-from .directory_management import create_directory_if_doesnt_exists, directory_exists
 from .parser import bool_parse
 from .pytorch_utils import weight_init
 from .timer import Timer, timed
