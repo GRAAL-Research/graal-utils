881161e38093aca684f93e703f114964f8fabcd4
David Beauchemin <david.beauchemin.5@ulaval.ca>
2020-07-09 11:36:36 -0400

    added directory management function and parser for bool (#3)
    * added directory management function and parser for bool
    
    * updated gitignore for pycharms bullshit
    
    * error in __init__, added weights init
    
    * Update __init__.py
    
    * Delete directory_management.py

--- graal_utils/__init__.py
+++ graal_utils/__init__.py
@@ -1,1 +1,3 @@
+from .parser import bool_parse
+from .pytorch_utils import weight_init
 from .timer import Timer, timed
---
+++ graal_utils/pytorch_utils.py
@@ -0,0 +1,77 @@
+import torch.nn as nn
+import torch.nn.init as init
+
+
+def weight_init(m):
+    '''
+    Function to initialize the weight of a layer.
+
+    Usage:
+        model = Model()
+        model.apply(weight_init)
+
+    References
+    ----------
+    .. [1] Ma, Xiaojian (2020/02). "A simple script for parameter initialization for PyTorch", GitHub Gist
+        <https://gist.github.com/jeasinema/ed9236ce743c8efaf30fa2ff732749f5>`_
+    '''
+    if isinstance(m, nn.Conv1d):
+        init.normal_(m.weight.data)
+        if m.bias is not None:
+            init.normal_(m.bias.data)
+    elif isinstance(m, nn.Conv2d):
+        init.xavier_normal_(m.weight.data)
+        if m.bias is not None:
+            init.normal_(m.bias.data)
+    elif isinstance(m, nn.Conv3d):
+        init.xavier_normal_(m.weight.data)
+        if m.bias is not None:
+            init.normal_(m.bias.data)
+    elif isinstance(m, nn.ConvTranspose1d):
+        init.normal_(m.weight.data)
+        if m.bias is not None:
+            init.normal_(m.bias.data)
+    elif isinstance(m, nn.ConvTranspose2d):
+        init.xavier_normal_(m.weight.data)
+        if m.bias is not None:
+            init.normal_(m.bias.data)
+    elif isinstance(m, nn.ConvTranspose3d):
+        init.xavier_normal_(m.weight.data)
+        if m.bias is not None:
+            init.normal_(m.bias.data)
+    elif isinstance(m, nn.BatchNorm1d):
+        init.normal_(m.weight.data, mean=1, std=0.02)
+        init.constant_(m.bias.data, 0)
+    elif isinstance(m, nn.BatchNorm2d):
+        init.normal_(m.weight.data, mean=1, std=0.02)
+        init.constant_(m.bias.data, 0)
+    elif isinstance(m, nn.BatchNorm3d):
+        init.normal_(m.weight.data, mean=1, std=0.02)
+        init.constant_(m.bias.data, 0)
+    elif isinstance(m, nn.Linear):
+        init.xavier_normal_(m.weight.data)
+        init.normal_(m.bias.data)
+    elif isinstance(m, nn.LSTM):
+        for param in m.parameters():
+            if len(param.shape) >= 2:
+                init.orthogonal_(param.data)
+            else:
+                init.normal_(param.data)
+    elif isinstance(m, nn.LSTMCell):
+        for param in m.parameters():
+            if len(param.shape) >= 2:
+                init.orthogonal_(param.data)
+            else:
+                init.normal_(param.data)
+    elif isinstance(m, nn.GRU):
+        for param in m.parameters():
+            if len(param.shape) >= 2:
+                init.orthogonal_(param.data)
+            else:
+                init.normal_(param.data)
+    elif isinstance(m, nn.GRUCell):
+        for param in m.parameters():
+            if len(param.shape) >= 2:
+                init.orthogonal_(param.data)
+            else:
+                init.normal_(param.data)
