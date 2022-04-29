b7a16ffc335e9a7906f6a295358339205cb32576
Jean-Samuel Leboeuf <jean-samuel.leboeuf@hotmail.com>
2022-04-29 12:13:01 -0400

    add func_to_cmd in init

--- graal_utils/__init__.py
+++ graal_utils/__init__.py
@@ -1,4 +1,4 @@
-from .parser import bool_parse
+from .parser import func_to_cmd, bool_parse, list_parse
 from .pytorch_utils import weight_init
 from .timer import Timer, timed
 from .matplotlib import save_and_close_figure
